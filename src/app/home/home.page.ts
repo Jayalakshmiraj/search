0a85551a9eb4e686ff04684cde09527127215a3a
Jayalakshmiraj <Jayalakshmiraj@gmail.com>
2019-05-14 14:27:35 +0530

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { DataService } from "../services/data.service";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"]
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  public searchTerm: string = "";
+  public items: any;
 
+  constructor(private dataService: DataService) {}
+
+  ngOnInit() {
+    this.setFilteredItems();
+  }
+
+  setFilteredItems() {
+    this.items = this.dataService.filterItems(this.searchTerm);
+  }
 }
